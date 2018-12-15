2ae65f1f76227d465d66206b8a949decb197908c
Alex Williams <Zizzs17@gmail.com>
2018-12-14 18:16:15 -0800

    Creation of 100 boids, shoots out at random velocities in random directions from center of canvas.

---
+++ boid.js
@@ -0,0 +1,19 @@
+class Boid {
+    constructor() {
+        this.position = createVector(width/2, height/2);
+        this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(0.5, 1.5));
+        this.acceleration = createVector();
+    }
+
+    update() {
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+    }
+
+    show() {
+        strokeWeight(16);
+        stroke(255);
+        point(this.position.x, this.position.y);
+    }
+}
